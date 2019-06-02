ed8943de7658f483dddfdf646d40e4e0cdfaa7ff
Vladimir Gubarev <vladimir.gubarev@itiviti.com>
2019-06-02 20:01:58 +0300

    Initial commit

---
+++ src/main/java/org/mpi/faust/FaustApplication.java
@@ -0,0 +1,13 @@
+package org.mpi.faust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaustApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaustApplication.class, args);
+    }
+
+}
