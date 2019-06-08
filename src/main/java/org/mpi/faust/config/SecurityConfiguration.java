8eff7ff532809cf5f45d5ac508707f2e162e45ee
Vladimir Gubarev <vladimir.gubarev@itiviti.com>
2019-06-08 14:39:46 +0300

    Init role management

--- src/main/java/org/mpi/faust/config/SecurityConfiguration.java
+++ src/main/java/org/mpi/faust/config/SecurityConfiguration.java
@@ -4,6 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
@@ -11,14 +28,24 @@ import org.springframework.security.web.savedrequest.SimpleSavedRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private FaustUserService userService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .oauth2Login().and()
+                .oauth2Login()
+                    .userInfoEndpoint()
+                        .userService(this.userService())
+                .and()
+                .and()
                 .csrf()
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and()
@@ -26,6 +53,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**/*.{js,html,css}").permitAll()
                 .antMatchers("/", "/api/v1/user").permitAll()
                 .anyRequest().authenticated();
+    }
+
+    private OAuth2UserService<OAuth2UserRequest, OAuth2User> userService() {
+        return userService;
     }
 
     @Bean
@@ -39,5 +70,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 }
             }
         };
+    }
+
+    private class FaustUserService extends DefaultOAuth2UserService {
+        public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+            OAuth2User user = super.loadUser(userRequest);
+            Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>() {
+            }; // = Collections.singleton(new OAuth2UserAuthority(user.getAttributes()));
+            // make bunch of authorities from groups
+            String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+            return new DefaultOAuth2User(authorities, user.getAttributes(), userNameAttributeName);
+        }
     }
 }
